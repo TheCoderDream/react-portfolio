584eff99b1e098d1b7bee40c027ed7b51ec14f4e
Emre <3mr3baskan@gmail.com>
2020-11-25 15:22:44 +0300

    * updated pdf resume

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -36,7 +36,7 @@ const Profile = () => {
                 <Skills />
             </div>
             <div className="profile__contact">
-                <a href="https://drive.google.com/file/d/1KS59x7gIOht-r6pZ3DaV5kDDxSsqXNML/view?usp=sharing" target="_blank" rel="noreferrer">
+                <a href="https://drive.google.com/file/d/1rzAuF9qNB0ZqHMFamNG_b00bW2oKoAya/view?usp=sharing" target="_blank" rel="noreferrer">
                         <span>
                             Download CV
                         </span>
